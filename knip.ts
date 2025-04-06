9fbf558dd515992d00f7ce9d05c3e92485b13b60
Alex Miller <codex.nz@gmail.com>
2025-04-06 12:43:00 +1200

    chore: update eslint packages and config

--- knip.ts
+++ knip.ts
@@ -3,11 +3,7 @@ import type { KnipConfig } from "knip";
 const config: KnipConfig = {
   ignore: ["dist/**"],
   ignoreDependencies: [
-    // Used in eslint.config.mjs
-    "eslint-plugin-github",
-    "eslint-plugin-import",
     // Required by eslint-plugin-import-x
-    "@typescript-eslint/parser",
     "eslint-import-resolver-typescript",
   ],
 };
