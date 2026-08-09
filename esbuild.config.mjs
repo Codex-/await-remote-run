783746e42dc3cb9a66a0e74002ad1f00b7607386
Alex Miller <codex.nz@gmail.com>
2026-08-10 10:12:23 +1200

    chore(build): remove top-level iife

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,41 +1,39 @@
 import chalk from "chalk";
 import { analyzeMetafile, build } from "esbuild";
 
-(async () => {
-  try {
-    const startTime = Date.now();
-    console.info(
-      chalk.bold(`🚀 ${chalk.blueBright("await-remote-run")} Build\n`),
-    );
+try {
+  const startTime = Date.now();
+  console.info(
+    chalk.bold(`🚀 ${chalk.blueBright("await-remote-run")} Build\n`),
+  );
 
-    const result = await build({
-      entryPoints: ["./src/index.ts"],
-      outfile: "dist/index.mjs",
-      metafile: true,
-      bundle: true,
-      format: "esm",
-      platform: "node",
-      target: ["node24"],
-      treeShaking: true,
-      // Ensure require is properly defined: https://github.com/evanw/esbuild/issues/1921
-      banner: {
-        js:
-          "import { createRequire as __await_remote_run_cr } from 'node:module';\n" +
-          "const require = __await_remote_run_cr(import.meta.url);",
-      },
-    });
+  const result = await build({
+    entryPoints: ["./src/index.ts"],
+    outfile: "dist/index.mjs",
+    metafile: true,
+    bundle: true,
+    format: "esm",
+    platform: "node",
+    target: ["node24"],
+    treeShaking: true,
+    // Ensure require is properly defined: https://github.com/evanw/esbuild/issues/1921
+    banner: {
+      js:
+        "import { createRequire as __await_remote_run_cr } from 'node:module';\n" +
+        "const require = __await_remote_run_cr(import.meta.url);",
+    },
+  });
 
-    const analysis = await analyzeMetafile(result.metafile);
-    console.info(`📝 Bundle Analysis:${analysis}`);
+  const analysis = await analyzeMetafile(result.metafile);
+  console.info(`📝 Bundle Analysis:${analysis}`);
 
-    console.info(
-      `${chalk.bold.green("✔ Bundled successfully!")} (${
-        Date.now() - startTime
-      }ms)`,
-    );
-  } catch (error) {
-    console.error(`🧨 ${chalk.red.bold("Failed:")} ${error.message}`);
-    console.debug(`📚 ${chalk.blueBright.bold("Stack:")} ${error.stack}`);
-    process.exit(1);
-  }
-})();
+  console.info(
+    `${chalk.bold.green("✔ Bundled successfully!")} (${
+      Date.now() - startTime
+    }ms)`,
+  );
+} catch (error) {
+  console.error(`🧨 ${chalk.red.bold("Failed:")} ${error.message}`);
+  console.debug(`📚 ${chalk.blueBright.bold("Stack:")} ${error.stack}`);
+  process.exit(1);
+}
